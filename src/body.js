769619516971ac7cc295a6ccd569f301e28b2322
haroldosales <haroldosales>
2024-05-05 20:11:54 -0300

    up

---
+++ src/body.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Body() {
+  return (
+    <div className="App">
+      <body className="App-body">
+        Teste 
+      </body>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function Footer() {
+  return (
+    <div className="App">
+      <footer className="App-footer">
+        @Hsa
+      </footer>
+    </div>
+  );
+}
+
+export default  Footer;
